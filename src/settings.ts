22a991e21ba1c5d30aa533f6763a71cbf9ffe3f9
Brandon Bayer <b@bayer.ws>
2020-12-03 18:25:28 -0500

    add Needs Defined column

--- src/settings.ts
+++ src/settings.ts
@@ -1,21 +1,22 @@
 export const TRIAGE_LABEL = "status/triage";
 export const IN_PROGRESS_LABEL = "status/in-progress";
 export const IN_REVIEW_LABEL = "status/in-review";
-export const ASSIGNED = "status/assigned"
+export const ASSIGNED = "status/assigned";
 
 export const LABEL_TO_COLUMN: Record<string, string> = {
   "status/icebox": "Icebox",
   "status/blocked": "Blocked",
   [TRIAGE_LABEL]: "Triage",
+  "status/needs-defined": "Needs Defined",
   "status/ready-to-work-on": "Ready to Work On",
   "status/assigned": "Assigned",
   [IN_PROGRESS_LABEL]: "In Progress",
   [IN_REVIEW_LABEL]: "In Review",
-  "status/done": "Done",
+  "status/done": "Done"
 };
 
 export const FILETYPE_TO_CONTRIB_TYPE: Array<Record<string, Array<string>>> = [
   { docs: [".md", ".txt"] },
   { test: [".test.js", ".test.ts"] },
-  { code: [".js", ".ts", ".jsx", ".tsx", ".json", ".sh"] },
+  { code: [".js", ".ts", ".jsx", ".tsx", ".json", ".sh"] }
 ];
