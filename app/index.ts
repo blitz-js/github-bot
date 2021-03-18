307a6ef7383a2bcb5d528a2e415dceeeb9af21c6
JuanM04 <me@juanm04.com>
2021-03-17 22:28:46 -0300

    Removed `pull_request.labeled` in favor of `issues.labeled`

--- app/index.ts
+++ app/index.ts
@@ -6,7 +6,6 @@ import {issuesLabeled} from "./events/issues-labeled"
 import {issuesOpened} from "./events/issues-opened"
 import {project_cardMoved} from "./events/project_card-moved"
 import {pull_requestClosed} from "./events/pull_request-closed"
-// import {pull_requestLabeled} from "./events/pull_request-labeled"
 import {pull_requestOpened} from "./events/pull_request-opened"
 import {repositoryCreated} from "./events/repository-created"
 import log from "./utils/log"
@@ -34,7 +33,6 @@ app.on(["issues.labeled", "pull_request.labeled"], issuesLabeled)
 app.on("issues.opened", issuesOpened)
 app.on(["project_card.moved", "project_card.created"], project_cardMoved)
 app.on("pull_request.closed", pull_requestClosed)
-// app.on("pull_request.labeled", pull_requestLabeled)
 app.on(["pull_request.opened", "pull_request.ready_for_review"], pull_requestOpened)
 
 // Organization events
